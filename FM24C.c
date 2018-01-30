283087277e0bdddd7d8a38441b3bdb9473f328fb
SMFSW <xgoinxpostalx@gmail.com>
2018-01-30 02:41:40 +0100

    v0.7: fix

--- FM24C.c
+++ FM24C.c
@@ -34,7 +34,7 @@ __WEAK FctERR FM24C_Init(void)
 
 	err = I2C_slave_init(&FM24C_hal, I2C_FM24C, FM24C_BASE_ADDR, I2C_slave_timeout);
 
-	if (err)	{ I2C_set_enable(&MB85RC256V_hal, false); }
+	if (err)	{ I2C_set_enable(&FM24C_hal, false); }
 
 	return ERROR_OK;
 }
