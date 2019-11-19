218d19239ae370465ba8d73ca57128a39ee9754f
Travis CI <travis@travis-ci.org>
2019-11-19 11:47:01 +0000

    Deploy code docs to GitHub Pages Travis build: 39
    Commit: 93382736951551ea30f3d545874afaacab03f33a

--- html/search/all_a.js
+++ html/search/all_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['key_5fin_5fcalib',['Key_In_Calib',['../d4/d1f/unionu_a_t42_q_t___r_e_g_____d_e_v_i_c_e___s_t_a_t_u_s.html#a670bf06a6aaf09d933e02b4f7a4dd20f',1,'uAT42QT_REG__DEVICE_STATUS']]],
   ['keydata',['KeyData',['../d1/db6/unionu_a_t42_q_t___r_e_g___m_a_p.html#aee8e164d0464185e7c5b099e353f7134',1,'uAT42QT_REG_MAP']]],
+  ['keys',['keys',['../da/dbc/struct_a_t42_q_t1244__t.html#a76f5032a10198e0110f356b1d5b049c4',1,'AT42QT1244_t']]],
   ['kgtt',['KGTT',['../d7/df7/unionu_a_t42_q_t___r_e_g_____s_e_t_u_p__243.html#af414051e62af2f99cbf4d71c046b25e3',1,'uAT42QT_REG__SETUP_243']]]
 ];
